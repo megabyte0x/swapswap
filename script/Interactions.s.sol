ca61977ab5d03813ec1c3aaa2a8b79908752cf94
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-10-14 15:33:49 +0530

    feat: Deployment Script for Factory

---
+++ script/Interactions.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployBTCSwapSwap is Script {}
